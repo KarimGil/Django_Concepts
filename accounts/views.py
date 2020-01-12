ecb84367a0ddfcbe83b5b8b39a3ff11234b50296
KarimGil <karimg87@hotmail.com>
2020-01-12 00:31:54 -0800

    working on user registration

---
+++ accounts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['fname']
+        last_name = request.POST['lname']
+        username = request.POST['uname']
+        email = request.POST['email']
+        password = request.POST['pass']
+        user = User.objects.create_user(username = username, password = password, first_name = first_name, last_name = last_name, email = email)
+        user.save()
+        print('user created')
+        return redirect("/")
+    else:
+        return render(request,'register.html')
